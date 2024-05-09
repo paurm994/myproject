4ec230e76d01c1a90322a74e008d57c00e39b83c
Pau Roqué <paurm994@gmail.com>
2024-05-09 12:29:29 +0200

    ejercicio 2 js

---
+++ learn-js/ej-2.js
@@ -0,0 +1,8 @@
+let lado = 40
+let area = lado * lado
+let perimetro = lado * 4
+document.write("<p>Con un lado de: " + lado + "</p>")
+document.write("<p>Tenemos una área de: " + area + "</p>")
+document.write("<p>Y un perímetro de: " + perimetro + "</p>")
+
+// document.getElementById("resultado").innerText = "El lado es: " + lado + ", el área es de: " + area + "y el perímetro es de: " + perimetro
