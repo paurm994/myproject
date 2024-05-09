d393081f15efdab44096f52b2f7a746be30c9347
Pau Roqué <paurm994@gmail.com>
2024-05-09 12:36:54 +0200

    unificar resultados JS en un solo archivo resultados.html

--- learn-js/ej-1.js
+++ learn-js/ej-1.js
@@ -2,4 +2,4 @@
 let precio = 200
 let iva = 21
 let precioTotal = precio + (iva * precio / 100)
-console.log("El precio con iva es:" + precioTotal + "€")
+document.write("El precio con iva es: " + precioTotal + "€")
