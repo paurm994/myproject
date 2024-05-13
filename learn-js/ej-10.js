04f95102111558929e4d9cf6676a656af11d72b5
Pau Roqué <paurm994@gmail.com>
2024-05-13 14:40:08 +0200

    añadir comentarios ejercicio 10 JS

--- learn-js/ej-10.js
+++ learn-js/ej-10.js
@@ -1,6 +1,10 @@
+// Pedimos un número al usuario y lo convertimos a entero
 let numeroUsuario = parseInt(prompt("Introduce un número"));
 
+// Calculamos el resto de la división. Si es 0 será par y sino, impar.
 let resto = numeroUsuario % 2;
+// Comprobamos si el resto es 0 (en cuyo caso será true) o no (será false)
 let par = resto == 0;
 
+// Escribimos el resultado en el documento
 document.write("¿El número es par? ->> " + par);
