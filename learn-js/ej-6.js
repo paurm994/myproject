c4f0318a1d5166791020c6acb5cfaf4d9dfcc7bd
Pau Roqué <paurm994@gmail.com>
2024-05-13 13:45:02 +0200

    correccion menor ej-6 JS

--- learn-js/ej-6.js
+++ learn-js/ej-6.js
@@ -1,9 +1,9 @@
-let horas = Number(prompt("Introduce la cantidad de HORAS"))
-let minutos = Number(prompt("Introduce la cantidad de MINUTOS"))
+let horas = Number(prompt("Introduce la cantidad de HORAS"));
+let minutos = Number(prompt("Introduce la cantidad de MINUTOS"));
 
-const SEGUNDOS_EN_HORA = horas * 60 * 60
-const SEGUNDOS_EN_MINUTOS = minutos * 60
+const SEGUNDOS_EN_HORA = horas * 60 * 60;
+const SEGUNDOS_EN_MINUTOS = minutos * 60;
 
-let segundosTotales = SEGUNDOS_EN_HORA + SEGUNDOS_EN_MINUTOS
+let segundosTotales = SEGUNDOS_EN_HORA + SEGUNDOS_EN_MINUTOS;
 
-document.write("En " + horas + " horas y " + minutos + " minutos, hay un total de " + segundosTotales + " segundos!")
+document.write("En " + horas + " horas y " + minutos + " minutos, hay un total de " + segundosTotales + " segundos!");
