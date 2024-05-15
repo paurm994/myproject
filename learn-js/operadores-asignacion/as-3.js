5149c6c9039b14fa745e0443d834d39ce18b9580
Pau Roqué <paurm994@gmail.com>
2024-05-15 14:32:30 +0200

    ejercicio 3 asignacion

---
+++ learn-js/operadores-asignacion/as-3.js
@@ -0,0 +1,30 @@
+let contador = 5;
+document.write("<p>Inicializamos el contador en 5: -> " + contador + "</p>");
+
+// Se puede hacer con un bucle:
+
+for (let i = 0; i < 5; i++) {
+    contador *= 2;
+}
+
+document.write("<p>Despues de 5 iteraciones del bucle (multiplicar el contador *2 5 veces), el contador vale: -> " + contador + "</p>");
+
+// // Multiplicamos el contador * 2
+// contador *= 2;
+// document.write("<p>Multiplicamos el contador * 2: -> " + contador + "</p>");
+
+// // Multiplicamos el contador * 2
+// contador *= 2;
+// document.write("<p>Multiplicamos el contador * 2: -> " + contador + "</p>");
+
+// // Multiplicamos el contador * 2
+// contador *= 2;
+// document.write("<p>Multiplicamos el contador * 2: -> " + contador + "</p>");
+
+// // Multiplicamos el contador * 2
+// contador *= 2;
+// document.write("<p>Multiplicamos el contador * 2: -> " + contador + "</p>");
+
+// // Multiplicamos el contador * 2
+// contador *= 2;
+// document.write("<p>Multiplicamos el contador * 2: -> " + contador + "</p>");
