8ecb080f5d4d53df8154df4305bbb0ffd24fd83b
Pau Roqué <paurm994@gmail.com>
2024-05-15 13:56:20 +0200

    ejercicio 1 asignación JS

---
+++ learn-js/operadores-asignacion/as-1.js
@@ -0,0 +1,14 @@
+let x = 10;
+document.write("<p>Iniciamos la variable con valor: " + x + "</p>");
+
+// Primera forma de incrementar 3:
+x = x + 3;
+document.write("<p>Lo incrementamos en 3 de la primera forma: x = x + 3 ->" + x + "</p>");
+
+// Volvemos a dejar la x con el valor inicial de 10:
+x = 10
+document.write("<p>Volvemos a dejar la x con el valor inicial: " + x + "</p>");
+
+// Segunda forma de incrementar 3:
+x += 3;
+document.write("<p>Lo incrementamos en 3 de la segunda forma: x += 3 -> " + x + "</p>");
