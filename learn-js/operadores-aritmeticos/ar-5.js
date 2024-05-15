7a15a26c3c7ea4b516173a84bfd8449f411d9f2e
Pau Roqué <paurm994@gmail.com>
2024-05-15 13:44:30 +0200

    ejercicio 5 aritmeticos

---
+++ learn-js/operadores-aritmeticos/ar-5.js
@@ -0,0 +1,8 @@
+document.write("<p>2 elevado a 3 = " + 2**3 + "</p>")
+
+document.write("<p>2 elevado a 0 = " + 2**0 + "</p>")
+
+document.write("<p>0 elevado a 0 = " + 0**0 + "</p>")
+
+// Observamos que las números elevados a 0 son 1.
+// El 0 elevado a 0 también es 1.
