84aafbdb64c1d2ddad2f694d1eb53c4ec5cf023d
Pau Roqué <paurm994@gmail.com>
2024-05-15 12:51:17 +0200

    correccion ejercicio 1 aritmetico

--- learn-js/operadores-aritmeticos/ar-1.js
+++ learn-js/operadores-aritmeticos/ar-1.js
@@ -17,10 +17,10 @@ valor4 = 3.14 * r **2;
 
 document.write("valor4 = " + valor4 + " ");
 
-let valor5 = 10 ** 3
+let valor5 = 10 ** 3;
 document.write("valor5 = " + valor5 + " ");
 
-let valor6 = 10 ** -3
+let valor6 = 10 ** -3;
 document.write("valor6 = " + valor6 + " ");
 
 // Poner ** delante de un número significa que lo elevas a la potencia.
