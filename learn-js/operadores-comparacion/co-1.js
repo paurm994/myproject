d0bb9de8170d78f9248609ba2a6addcb34d42e9e
Pau Roqué <paurm994@gmail.com>
2024-05-15 15:25:32 +0200

    ejercicio 1 comparacion

---
+++ learn-js/operadores-comparacion/co-1.js
@@ -0,0 +1,18 @@
+
+let comparacion1 = 7 > 3;
+document.write("<p>La comparación de 7 > 3 es -> " + comparacion1 + "</p>");
+
+let comparacion2 = 3 < 7;
+document.write("<p>La comparación de 3 < 7 es -> " + comparacion2 + "</p>");
+
+let comparacion3 = 1 == '1';
+document.write("<p>La comparación de 1 == '1' es -> " + comparacion3 + "</p>");
+
+let comparacion4 = 1 === '1';
+document.write("<p>La comparación de 1 === '1' es -> " + comparacion4 + "</p>");
+
+let comparacion5 = 3 != '3';
+document.write("<p>La comparación de 3 != '3' es -> " + comparacion5 + "</p>");
+
+let comparacion6 = 3 !== '3';
+document.write("<p>La comparación de 3 !== '3' es -> " + comparacion6 + "</p>");
