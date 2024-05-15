b6b4f152bf30331942ab672ae6abad9ef2d9baf0
Pau Roqué <paurm994@gmail.com>
2024-05-15 12:47:26 +0200

    ejercicio 1 operadores aritmeticos

---
+++ learn-js/operadores-aritmeticos/ar-1.js
@@ -0,0 +1,27 @@
+let valor1, valor2, valor3, valor4 = 0;
+valor1 = 10 + 5 * 2;
+
+document.write("valor1 = " + valor1 + " ");
+
+valor2 = (10 + 5) * 2;
+document.write("valor2 = " + valor2 + " ");
+
+valor3 = 10 + (5 * 2);
+document.write("valor3 = " + valor3 + " ");
+
+// Se observa que lo que va entre parentesis se calcula antes.
+// La multiplicación también se calcula antes que la suma o la resta.
+
+let r = 10;
+valor4 = 3.14 * r **2;
+
+document.write("valor4 = " + valor4 + " ");
+
+let valor5 = 10 ** 3
+document.write("valor5 = " + valor5 + " ");
+
+let valor6 = 10 ** -3
+document.write("valor6 = " + valor6 + " ");
+
+// Poner ** delante de un número significa que lo elevas a la potencia.
+// 10 ** 3 significa 10 elevado a 3.
