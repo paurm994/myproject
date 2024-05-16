94f9c36f3ff538cbdedf15d04b977c1e767bcbea
Pau Roqué <paurm994@gmail.com>
2024-05-16 13:56:13 +0200

    Añado comentario en ejercicio 1 comparación JS

--- learn-js/operadores-comparacion/co-1.js
+++ learn-js/operadores-comparacion/co-1.js
@@ -6,10 +6,10 @@ let comparacion2 = 3 < 7;
 document.write("<p>La comparación de 3 < 7 es -> " + comparacion2 + "</p>");
 
 let comparacion3 = 1 == '1';
-document.write("<p>La comparación de 1 == '1' es -> " + comparacion3 + "</p>");
+document.write("<p>La comparación de 1 == '1' es -> " + comparacion3 + "</p>"); // Se compara si los valores coinciden. Comparación debil.
 
 let comparacion4 = 1 === '1';
-document.write("<p>La comparación de 1 === '1' es -> " + comparacion4 + "</p>");
+document.write("<p>La comparación de 1 === '1' es -> " + comparacion4 + "</p>"); // Se compara si el tipo de dato y el valor coinciden, comparación estricta.
 
 let comparacion5 = 3 != '3';
 document.write("<p>La comparación de 3 != '3' es -> " + comparacion5 + "</p>");
