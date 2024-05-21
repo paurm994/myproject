e2799560e9e76d6e33b7887cf6adabf805eb7b6f
Pau Roqué <paurm994@gmail.com>
2024-05-21 15:09:52 +0200

    ejercicio 3 strings JS

---
+++ learn-js/operadores-string/str-3.js
@@ -0,0 +1,19 @@
+// Concatena:
+// Dos números, la suma y el resultado
+let num1 = 13;
+let num2 = 14;
+let suma = num1 + num2;
+document.write("<p>El resultado de sumar " + num1 + " y " + num2 + " es: " + suma + "</p>");
+
+
+// Tres números, el promedio y el resultado
+let num3 = 8;
+let num4 = 29;
+let num5 = 94;
+let promedio = (num3 + num4 + num5) / 3;
+document.write("<p> El promedio de los números " + num3 + " , " + num4 + " y " + num5 + " es: " + promedio + "</p>");
+
+//Un número, la raiz cuadrada y el resultado
+let num6 = 34;
+let raizCuadrada = Math.sqrt(num6);
+document.write("<p> La raíz cuadrada de " + num6 + " es: " + raizCuadrada + "</p>");
