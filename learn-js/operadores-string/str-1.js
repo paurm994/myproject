4c198af66c3f4c9d42fe3a95adcb10b5edda00d5
Pau Roqué <paurm994@gmail.com>
2024-05-21 14:45:19 +0200

    corrección en los ejercicios: unificar ejercicio 2 y 3 en ejercicio 1 ya que eran parte del 1, y cambiar el nombre de ejercicio 4 a ejercicio 2 ya que realmente era el 2.

--- learn-js/operadores-string/str-1.js
+++ learn-js/operadores-string/str-1.js
@@ -1,3 +1,11 @@
 let nombreUsuario = "Juan Antonio";
-
 document.write("<p>Hola, " + nombreUsuario + "</p>");
+
+let horaActual = new Date().getHours();
+document.write("<p>La hora actual es: " + horaActual + "h </p>");
+
+
+let num1 = 15;
+let num2 = 69;
+let resultado = num1 + num2;
+document.write("<p>El resultado de la suma de " + num1 + " y " + num2 + " es: " + resultado + "</p>");
--- learn-js/operadores-string/str-3.js
+++
@@ -1,5 +0,0 @@
-let num1 = 15;
-let num2 = 69;
-let resultado = num1 + num2;
-
-document.write("<p>El resultado de la suma de " + num1 + " y " + num2 + " es: " + resultado + "</p>");
