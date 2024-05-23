9ebcdb58bd7b8ac6c19a22b4ba2869604c5f9445
Pau Roqué <paurm994@gmail.com>
2024-05-23 12:18:33 +0200

    pequeño cambio en funcion 2

--- learn-js/aulaScript/funcion-2.js
+++ learn-js/aulaScript/funcion-2.js
@@ -2,7 +2,7 @@ function esPar(num){
     return(num % 2 == 0);
 }
 
-const numeroUsuario = prompt("Dame un número y te digo true si es par y false si no lo es");
+const numeroUsuario = parseInt(prompt("Dame un número y te digo true si es par y false si no lo es"));
 
 document.write("<p>El número del usuario es: " + numeroUsuario + "</p>");
 
